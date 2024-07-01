9538c244910ea46e64c72f7f8a0aca7e6c199024
沈耀豪 <2959278693@qq.com>
2024-07-01 09:36:45 +0800

    第一次提交

---
+++ src/main/java/com/syh/gitdemo1/GitDemo1Application.java
@@ -0,0 +1,13 @@
+package com.syh.gitdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/syh/gitdemo1/GitDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syh.gitdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
